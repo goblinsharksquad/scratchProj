cf9ef3795b79a7f004bb14e7945230923b9f1d24
“Tiara <tiaravan234@gmail.com>
2024-12-18 16:11:17 -0500

    signup and login

---
+++ frontend/src/LandingPage/Signup.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function SignUp() {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('Signup submitted', name, email,password, confirmPassword);
+  };
+
+  return (
+    <div>
+      <form className='signUp' onSubmit={handleSubmit}>
+        <input
+          type='name'
+          placeholder='Name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        ></input>
+        <input
+          type='email'
+          placeholder='Enter email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        ></input>
+        <input
+          type='password'
+          placeholder='Enter password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        ></input>
+        <input
+          type='password'
+          placeholder='Enter confirm password'
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        ></input>
+        <button type='submit'>Log in</button>
+      </form>
+      <p>
+        Already have an account? <Link to='/Login'>Log In</Link>
+      </p>
+    </div>
+  );
+}
