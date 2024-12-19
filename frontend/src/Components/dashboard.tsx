2130ab6ec1fd9e02acab040726fe56b1bc6689f4
Christopher Yhap <christopheryhap@ChristophersAir.lan>
2024-12-19 15:54:43 -0500

    Dashboard

--- frontend/src/Components/dashboard.tsx
+++ frontend/src/Components/dashboard.tsx
@@ -12,21 +12,21 @@ class Dash extends Component {
         </div>
         <div className='options'>
           <div className='events'>
-            <a href='#' className='item1'>
+            <a href='#' className='item'>
               Events
             </a>
             <p>Check out what events are baking in Santa Village. </p>
           </div>
 
           <div className='groups'>
-            <a href='#' className='item2'>
+            <a href='#' className='item'>
               Groups
             </a>
             <p>Connect with other Elves and be merry.</p>
           </div>
 
           <div className='events'>
-            <a href='#' className='item1'>
+            <a href='#' className='item'>
               Participants
             </a>
             <p>Check out all the other hardworking elves in Santa's Village.</p>
