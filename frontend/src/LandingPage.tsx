fbef452f34c274f373b90471bc0dde46f38d5e20
“Tiara <tiaravan234@gmail.com>
2024-12-19 16:55:41 -0500

    profile route

--- frontend/src/LandingPage.tsx
+++ frontend/src/LandingPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Login from './Components/Login';
 import SignUp from './Components/Signup';
+import ProfilePage from './ProfilePage';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 const LandingPage = () => {
@@ -12,6 +13,7 @@ const LandingPage = () => {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/Signup' element={<SignUp />} />
+          <Route path='/profile' element={<ProfilePage />} />
         </Routes>
       </div>
     </Router>
