0afb26556b328de79d00a310be87945565181702
Christopher Yhap <christopheryhap@ChristophersAir.lan>
2024-12-19 15:42:15 -0500

    Dashboard Component

--- frontend/src/Components/dashboard.tsx
+++ frontend/src/Components/dashboard.tsx
@@ -1,39 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './dashboar.css';
 
-const Dash = () => {
-  return (
-    <>
-      <div className='dashboard'>
-        <div className='header'>
-          <h1>DASHBOARD</h1>
+class Dash extends Component {
+  render() {
+    return (
+      <>
+        <div className='dashboard'>
+          <div className='header'>
+            <h1>DASHBOARD</h1>
+          </div>
         </div>
-      </div>
-      <div className='options'>
-        <div className='events'>
-          <a href='#' className='item1'>
-            Events
-          </a>
-          <p>Check out what events are baking in Santa Village. </p>
-        </div>
+        <div className='options'>
+          <div className='events'>
+            <a href='#' className='item1'>
+              Events
+            </a>
+            <p>Check out what events are baking in Santa Village. </p>
+          </div>
 
-        <div className='groups'>
-          <a href='#' className='item2'>
-            Groups
-          </a>
-          <p>Connect with other Elves and be merry.</p>
-        </div>
+          <div className='groups'>
+            <a href='#' className='item2'>
+              Groups
+            </a>
+            <p>Connect with other Elves and be merry.</p>
+          </div>
 
-        <div className='events'>
-          <a href='#' className='item1'>
-            Participants
-          </a>
-          <p>Check out all the other hardworking elves in Santa's Village.</p>
+          <div className='events'>
+            <a href='#' className='item1'>
+              Participants
+            </a>
+            <p>Check out all the other hardworking elves in Santa's Village.</p>
+          </div>
         </div>
-      </div>
-    </>
-  );
-};
-
+      </>
+    );
+  }
+}
 
 export default Dash;
